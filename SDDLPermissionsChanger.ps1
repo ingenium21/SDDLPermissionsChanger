477907c20c822aa544d1542ac3d0d34232d82462
Renato Regalado <renato.regalado@gmail.com>
2019-11-20 17:05:52 -0600

    finally Made Progress!

--- SDDLPermissionsChanger.ps1
+++ SDDLPermissionsChanger.ps1
@@ -1,11 +1,55 @@
-#Get Services
+$wanted_sids = @(
+    "Everyone",
+    "Authenticated Users",
+    "Domain Users",
+    "Users"
+) | ForEach-Object -Process {
+    $account = New-Object -TypeName System.Security.Principal.NTAccount -ArgumentList $_
+    $account.Translate([System.Security.Principal.SecurityIdentifier]).Value
+}
 
-$services = Get-Service
+$RightsToCheck = 0xc0002  # ChangeConfig, ChangePermissions, ChangeOwner
+
+$services = Get-Service 
 
 foreach ($s in $services){
-    [String] $Sddl = sc.exe sdshow $s.Name
+    $ServiceName = $s.Name
+    $ServiceName = $s.Name
+    [String] $Sddl = sc.exe sdshow $ServiceName
 
-    $Header = $Sddl -split '([A-Z]:)'[1]
-    "The Headers for $s are:"
-    Write-Host $Header
+    #attempt to create a security descriptor object
+    try {
+        $SD = New-Object System.Security.AccessControl.CommonSecurityDescriptor(
+            $false,  # Not a container
+            $false,  # Not a DS Object
+            $Sddl
+        )
+    }
+
+    catch {
+        Write-Warning ("Error creating security descriptor for {0}: {1}" -f $ServiceName, $_.Exception.Message)
+    }
+
+    foreach ($sid in $wanted_sids){
+        if ($SD.DiscretionaryAcl | where { $_.AceQualifier -eq [System.Security.AccessControl.AceQualifier]::AccessAllowed -and $_.SecurityIdentifier -eq $sid -and $_.AccessMask -band $RightsToCheck }) {
+            $null = $SD.DiscretionaryAcl.RemoveAccess(
+                "Allow",   # ACE type
+                $sid,
+                $RightsToCheck,
+                "ContainerInherit, ObjectInherit", # InheritanceFlags
+                "None"  #PropagationFlags
+            )
+    
+            [PSCustomObject] @{
+                Service = $ServiceName
+                SID = $sid
+                OriginalSddl = $Sddl
+                UpdatedSddl = $SD.GetSddlForm("All")
+            }
+
+            #sc.exe sdsset $ServiceName "$"
+        }
+    }
+
 }
+
